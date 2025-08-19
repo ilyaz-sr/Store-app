8c159dce6358b5cf16e938fc10ef528013c46e4a
ilyaz-sr <DRIVERsf@2018>
2025-08-19 10:59:11 +0330

    first commit to my repo3

--- src/main/java/com/codewithmosh/store/config/StripeConfig.java
+++ src/main/java/com/codewithmosh/store/config/StripeConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StripeConfig {
-    @Value("sk_test_51Rt3q6RrEOR3LONyXyJycoN3s7gZS9sDAqmDfgvGXQuZkbqWdatQ61t6v6t5O81TTvy30xmRi38bwS2MmUdVMZ5a00FbO6tDM3")
     private String secretKey;
 
     @PostConstruct
